10cd576ed09f111a8d1ca855f3aac3e03277a918
Your Name <akram.dcme@gmail.com>
2025-07-13 10:28:45 +0530

    Designed Mobile responsive

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
     initScrollEffects();
     initAdvancedCardStacking();
     initSkillAnimations();
-    initContactForm();
     initScrollIndicator();
     initParallaxEffects();
     initTypewriterEffect();
